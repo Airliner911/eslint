1ae8c2226a1a9bc77b91a07e7a7c41a7f2e781b5
Laksman <Airliner911@gmail.com>
2020-06-11 14:34:41 +0800

    test

---
+++ linttest/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+let  user ="welcome"
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello {user}!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+ 
+
+
